b0428a6e19423e60fa69c491dfee7407e82babc1
Shammya Saha <shammya.saha@asu.edu>
2025-01-21 15:14:24 -0500

    Initial Updates Made for fixing the map application

--- js/app.js
+++ js/app.js
@@ -57,8 +57,8 @@ function findMyCoordinates(){
     }
 }
 
-window.onload = function Function() {
-    findMyCoordinates();
+window.onload = async function Function() {
+    await findMyCoordinates();
     Papa.parse('epriDocuments.csv',{
         download: true,
         header: true,
@@ -79,8 +79,25 @@ window.onload = function Function() {
                 deviceIDs[i] = String(arr[i].ID);
                 i++
             }
+
+            if(document.getElementById('search')){
+                let select = document.getElementById("search");
+                while (select.firstChild) {
+                    select.removeChild(select.firstChild);
+                }
+                let placeholderOption = new Option("Select Device", "", true, true);
+                select.add(placeholderOption);
+                var i = 0;
+                while(i < deviceIDs.length){
+                    let newOption = new Option(deviceIDs[i], deviceIDs[i]);
+                    select.add(newOption);
+                    i++
+                }
+            }
         }
     });
+
+
 }
 
 if(document.getElementById('scan')){
@@ -142,7 +159,7 @@ function distance(lg, lt, dlg, dlt) {
 
 function selectDevice(data){
     var i = 0;
-    const selection = document.getElementById('selectedDocument');
+    // const selection = document.getElementById('selectedDocument');
     var optiontxt;
     const element = document.getElementById("files1");
     while (element.firstChild) {
@@ -153,6 +170,7 @@ function selectDevice(data){
             let link = document.createElement("a");
             let text = document.createTextNode(arr2[i].documents);
             link.href = arr2[i].documents;
+            link.target="_blank"
             link.appendChild(text);
             element.appendChild(link);
             element.appendChild(document.createElement("br"));
@@ -163,73 +181,77 @@ function selectDevice(data){
 }
 
 
-const submit = document.getElementById('submit').addEventListener('click', () => {
-    console.log("attempting to search")
+
+document.getElementById('submit').addEventListener('click', () => {
+    console.log("Attempting to search...");
     const userInput = document.getElementById('search').value;
+
     let i = 0;
     let j = 0;
-    let found = false
+    let found = false;
     const element = document.getElementById("files");
-
-    while(!found){
-        if(String(arr[i].ID) == userInput){
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+    
+    // Search in arr and display details
+    while (!found) {
+        if (String(arr[i].ID) === userInput) {
             document.getElementById('idVar').innerText = userInput;
             document.getElementById('long').innerText = arr[i].Longitude;
             document.getElementById('lat').innerText = arr[i].Latitude;
             document.getElementById('type').innerText = arr[i].type;
-            while(!found){
-                if(arr2[j].type==arr[i].type){
+
+            // Search in arr2 and display linked documents
+            let foundLink = false;
+            while (!foundLink) {
+                if (arr2[j].type === arr[i].type) {
                     let link = document.createElement("a");
                     let text = document.createTextNode(arr2[j].documents);
                     link.href = arr2[j].documents;
+                    link.target="_blank"
                     link.appendChild(text);
                     element.appendChild(link);
+                    element.appendChild(document.createElement("br"));
                     link.classList.add('link-primary');
                     
-                    found = true;
+                    // foundLink = true;
                 }
                 j++;
             }
+
+            // Generate CSV data (if needed, you can do something with it, e.g., download or display it)
             var csv = Papa.unparse({
-                "fields": ["ID","Latitude","Longitude","type"],
+                "fields": ["ID", "Latitude", "Longitude", "type"],
                 "data": [
                     [userInput, arr[i].Latitude, arr[i].Longitude, arr[i].type]
                 ]
             });
+
+            found = true; // End the while loop once the item is found
         }
         i++;
     }
-});
 
+    // Popup and map update
+    if (found) {
+        let popup = L.popup()
+            .setLatLng([arr[i-1].Latitude, arr[i-1].Longitude]) // Use i-1 to get the last valid index
+            .setContent('<h1>' + arr[i-1].ID + " Type: " + arr[i-1].type + '</h1>')
+            .openOn(map);
 
-const show = document.getElementById('show').addEventListener('click', () => {
-    const userInput = document.getElementById('search').value;
+        L.marker([arr[i-1].Latitude, arr[i-1].Longitude], {
+            icon: L.mapbox.marker.icon({
+                'marker-size': 'large',
+                'marker-color': '#023020'
+            })
+        }).bindPopup(popup).openPopup().addTo(map);
 
-    let i = 0;
-    let found = false;
-
-    console.log(userInput);
-    var popup = L.popup()
-                .setLatLng([arr[i].Latitude, arr[i].Longitude])
-                .setContent('<h1>' + arr[i].ID + " Type: " + arr[i].type + '</h1>')
-                .openOn(map);
-                    
-    while(!found){
-        if(String(arr[i].ID) == userInput){
-            L.marker([arr[i].Latitude, arr[i].Longitude], {
-                icon: L.mapbox.marker.icon({
-                    'marker-size': 'large',
-                    'marker-color': '#023020'
-                })
-            }).bindPopup(popup).openPopup().addTo(map);
-            found = true;
-            
-            map.setView([arr[i].Latitude, arr[i].Longitude],16);
-        }
-        i++;
+        map.setView([arr[i-1].Latitude, arr[i-1].Longitude], 16);
+        document.getElementById("map").focus();
     }
-    document.getElementById("map").focus();
 });
+
 
 
 function copyCoord() {
